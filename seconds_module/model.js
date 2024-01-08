fd851c4d03944dabe53459b99affe39cb7daaa6e
damthoaitin1004 <damthoaitin100041997@gmail.com>
2024-01-08 15:55:09 +0700

    [TinDT]

--- seconds_module/model.js
+++ seconds_module/model.js
@@ -1,34 +1,23 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
-const sequelize = new Sequelize('book_store', 'root', '123456',
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
+const {Sequelize,DataTypes,Model} = require('sequelize');
+const sequelize = new Sequelize('book_store','root','123456',
+{
+    host:'localhost',
+    dialect:'mysql'
+}
 );
-const Book = sequelize.define('Book', {
+const Book = sequelize.define('User', {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement:true
     },
     tittle: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull:false
     },
-    quantity: {
+    quantity:{
         type: DataTypes.BIGINT,
-        allowNull: false
-    }
-});
-const Author = sequelize.define('Author', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        type: DataTypes.STRING,
         allowNull:false
     }
-})
+  });
